8e55b45175b6ae84f7299b8ef5aa489c73e2289f
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-07 01:41:13 +0200

    Add middleware to store configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from './middleware/logger';
+// import logger from './middleware/logger';
 import reducer from './reducer';
+import func from './middleware/func';
+import api from './middleware/api';
 
 export default configureStore({
   reducer,
-  middleware: [logger],
+  middleware: [
+    // logger,
+    func,
+    api,
+  ],
 });
