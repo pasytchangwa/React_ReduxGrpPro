417980e3d68a7e8641fffaccbe773c45cd077f44
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-07 01:39:57 +0200

    Add funcional component with useEfect to aquire data from redux

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadRockets } from '../store/rockets';
 
-function Rockets() {
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { list } = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    dispatch(loadRockets());
+  }, []);
+
   return (
     <div>
       <h1>Rockets</h1>
+      <ul>
+        {list.map((rocket) => (
+          <li key={rocket.id}>
+            <h3>{rocket.rocket_name}</h3>
+            <p>{rocket.description}</p>
+            <img src={rocket.flickr_images} alt={rocket.rocket_name} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default Rockets;
