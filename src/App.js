b56642ed04fd3b8a83547f93b825b17dc298582d
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-07 01:40:33 +0200

    Refactor code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
-import { loadRockets } from './store/rockets';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
-
-store.dispatch(loadRockets());
 
 function App() {
   return (
