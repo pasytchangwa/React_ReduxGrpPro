a8f62d961646664aef4bd12de35bff52326cb831
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-07 02:03:52 +0200

    Add params to logger

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -1,7 +1,9 @@
-const logger = (store) => (next) => (action) => {
-  console.log('Store: ', store);
-  console.log('Next: ', next);
-  console.log('Action: ', action);
+const logger = (param) => (store) => (next) => (action) => {
+  console.log('Logging', param);
+  console.log('Store', store);
+  console.log('Next', next);
+  console.log('Action', action);
+  return next(action);
 };
 
 export default logger;
