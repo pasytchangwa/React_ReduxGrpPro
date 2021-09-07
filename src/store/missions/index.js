e9f81fb9b928ea2b30586681fad1b3d1de242960
Sylvestre <pasytchangwa@gmail.com>
2021-09-07 12:34:05 +0300

    Add logic and reducer of missions

---
+++ src/store/missions/index.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
+import { createSlice } from '@reduxjs/toolkit';
+import { apiCallBegan } from '../api';
+
+export const missionSlice = createSlice({
+  name: 'missions',
+  initialState: {
+    list: [],
+    loading: false,
+  },
+  reducers: {
+    missionRequested: (mission) => {
+      mission.loading = true;
+    },
+
+    missionReceived: (mission, action) => {
+      mission.list = action.payload;
+      mission.loading = false;
+    },
+
+    missionRequestFailed: (mission) => {
+      mission.loading = false;
+    },
+  },
+});
+
+export const { missionRequested, missionReceived, missionRequestFailed } =
+  missionSlice.actions;
+export default missionSlice.reducer;
+
+export const loadMissions = () => (dispatch) => {
+  const url = '/missions';
+  return dispatch(
+    apiCallBegan({
+      url,
+      onStart: missionRequested.type,
+      onSuccess: missionReceived.type,
+      onError: missionRequestFailed.type,
+    })
+  );
+};
