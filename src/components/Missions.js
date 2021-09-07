0128f993ba4d9f3206ee6b6e33d517c56789c4ae
Sylvestre <pasytchangwa@gmail.com>
2021-09-07 12:38:24 +0300

    Display missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadMissions } from '../store/missions';
 
-function Missions() {
+const Missions = () => {
+  const dispatch = useDispatch();
+  const { list } = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    dispatch(loadMissions());
+  }, []);
+
   return (
     <div>
       <h1>Missions</h1>
+      <ul>
+        {list.map((mission) => (
+          <li key={mission.id}>
+            <div className="card">
+              <h3>{mission.mission_name}</h3>
+              <p>{mission.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default Missions;
