3e00e8b6a569defbd6487b3613b6f33d7b0a70a8
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-07 02:02:45 +0200

    Adding ability for logger to accept params

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import logger from './middleware/logger';
 import reducer from './reducer';
+// import logger from './middleware/logger';
 import func from './middleware/func';
 import api from './middleware/api';
 
 export default configureStore({
   reducer,
   middleware: [
-    // logger,
+    // logger({ destination: 'console' }),
     func,
     api,
   ],
