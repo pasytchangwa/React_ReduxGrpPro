c24ac5d51f777c08d7eecce1fba7176a5a9302e7
Sylvestre <pasytchangwa@gmail.com>
2021-09-08 11:44:57 +0300

    Add Missions Mock test component

---
+++ src/components/__mocks__/Missions.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Missions = () => (
+  <div className="grid border-botom my-1">
+    <div className="text-bold align-top">Thaicom</div>
+    <div className="pb">
+      Thaicom is the name of a series of communications satellites operated from
+      Thailand, and also the name of Thaicom Public Company Limited, which is
+      the company that owns and operates the Thaicom satellite fleet and other
+      telecommunication businesses in Thailand and throughout the Asia-Pacific
+      region. The satellite projects were named Thaicom by the King of Thailand,
+      His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage
+      between Thailand and modern communications technology.
+    </div>
+    <div>
+      <div />
+      <div />
+    </div>
+    <div>
+      <button type="button" className='btn outline-secondary btn-sm'>
+        Join Mission'
+      </button>
+    </div>
+  </div>
+);
+
+export default Missions;
